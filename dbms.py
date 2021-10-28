813cf57e95eb6ccca735cf67ed8a1103c1f338ab
Sudheer <52126018+Cricsudheer@users.noreply.github.com>
2021-10-28 20:08:31 +0530

    Final Commit

--- dbms.py
+++ dbms.py
@@ -1,12 +1,27 @@
 import mysql.connector
+import  users
 
+#connecting to our database using python
 
-#for connecting to our database using python
-
-lib_database = mysql.connector.connect(
+flight_manager = mysql.connector.connect(
     host="localhost",
     user="root",
     password="Sudheer123#",
     database='Flight-Management-System',
 )
 
+#cursor for traversing in the database
+
+
+my_cursor = flight_manager.cursor()
+
+
+#passing user and admin objects
+
+def give_user_admin():
+    user = users.users(flight_manager)
+    admin = users.admins(flight_manager)
+
+    me =user
+    sup = admin
+    return [me, sup]
